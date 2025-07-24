bf720657ab28bc3867a917c0082275d243d89c03
Zac Montuori <zacmontuori@gmail.com>
2025-07-24 14:16:19 -0400

    CLI: switch to subcommand based interface
    - Add `standings` subcommand with -d/-c flags.
    - Require subcommand when running.
    - Drop print when no command given behavior.
    - Simplify main to match subcommand and call run function.

--- src/cli.rs
+++ src/cli.rs
@@ -1,18 +1,38 @@
-use clap::Parser;
+use clap::{ArgGroup, Args, Parser, Subcommand};
 
 /// F1 Standings CLI
 #[derive(Parser, Debug)]
-#[command(author, version, about)]
+#[command(
+    author,
+    version,
+    about,
+    subcommand_required = true,
+    arg_required_else_help = true
+)]
 pub struct Cli {
-    /// Show constructor standings for the current season.
-    #[arg(short = 'c', long = "cstandings")]
-    pub constructor_standings: bool,
+    #[command(subcommand)]
+    pub command: Command,
+}
 
-    /// Show driver standings for the current season.
-    #[arg(short = 'd', long = "dstandings")]
-    pub driver_standings: bool,
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    #[command(alias = "s")]
+    Standings(StandingsArgs),
+}
 
-    /// Season to query (current or four digit year).
+#[derive(Debug, Args)]
+#[command(
+    group(
+        ArgGroup::new("standings").args(["drivers", "constructors"]).required(true).multiple(false)
+)
+)]
+pub struct StandingsArgs {
+    #[arg(short = 'd', long = "drivers")]
+    pub drivers: bool,
+
+    #[arg(short = 'c', long = "constructors")]
+    pub constructors: bool,
+
     #[arg(short = 's', long = "season", default_value = "current")]
     pub season: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,16 @@
 mod api;
 mod cli;
+mod commands;
 mod models;
 mod output;
 
-use cli::Cli;
+use api::AppResult;
+use cli::{Cli, Command};
 
-fn main() {
+fn main() -> AppResult<()> {
     let args = Cli::parse();
 
-    // default: if no flag is supplied, just print both.
-    let print_driver_standings =
-        args.driver_standings || (!args.driver_standings && !args.constructor_standings);
-    let print_constructor_standings =
-        args.constructor_standings || (!args.driver_standings && !args.constructor_standings);
-
-    if print_driver_standings {
-        api::fetch_driver_standings(&args.season).map_or_else(
-            |e| {
-                eprintln!(
-                    "Error fetching driver standings for {} season: {}",
-                    args.season, e
-                )
-            },
-            |standings| output::print_driver_standings_table(&standings),
-        );
-    }
-    if print_constructor_standings {
-        api::fetch_constructor_standings(&args.season).map_or_else(
-            |e| {
-                eprintln!(
-                    "Error fetching constructor standings for {} season: {}",
-                    args.season, e
-                )
-            },
-            |standings| output::print_constructor_standings_table(&standings),
-        );
+    match args.command {
+        Command::Standings(a) => commands::run_standings(&a),
     }
 }
