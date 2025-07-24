d5ec43b88574b85250dfa5d9cc5d8017bf1aada4
Zac Montuori <zacmontuori@gmail.com>
2025-07-24 10:46:17 -0400

    Handle errors directly in main

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ mod cli;
 mod models;
 mod output;
 
-use api::AppResult;
 use cli::Cli;
 
-fn main() -> AppResult<()> {
+fn main() {
     let args = Cli::parse();
 
     // default: if no flag is supplied, just print both.
@@ -16,14 +15,25 @@ fn main() -> AppResult<()> {
         args.constructor_standings || (!args.driver_standings && !args.constructor_standings);
 
     if print_driver_standings {
-        let standings = api::fetch_driver_standings(&args.season)?;
-        output::print_driver_standings_table(&standings);
+        api::fetch_driver_standings(&args.season).map_or_else(
+            |e| {
+                eprintln!(
+                    "Error fetching driver standings for {} season: {}",
+                    args.season, e
+                )
+            },
+            |standings| output::print_driver_standings_table(&standings),
+        );
     }
-
     if print_constructor_standings {
-        let standings = api::fetch_constructor_standings(&args.season)?;
-        output::print_constructor_standings_table(&standings);
+        api::fetch_constructor_standings(&args.season).map_or_else(
+            |e| {
+                eprintln!(
+                    "Error fetching constructor standings for {} season: {}",
+                    args.season, e
+                )
+            },
+            |standings| output::print_constructor_standings_table(&standings),
+        );
     }
-
-    Ok(())
 }
