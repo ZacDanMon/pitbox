19ad0888d165e86e52aa10bf4b80f9ba5aceeaf5
Zac Montuori <zacmontuori@gmail.com>
2025-09-24 14:42:46 -0400

    Move reused table logic to a function

--- src/output.rs
+++ src/output.rs
@@ -17,20 +17,8 @@ static FLAGS: LazyLock<HashMap<String, String>> = LazyLock::new(|| {
 });
 
 /// Print a pretty formatted table of F1 driver standings to stdout.
-///
-/// # Arguments
-///
-/// * 'standings' - slice of DriverStanding struct (position, driver info, points)
 pub fn print_driver_standings_table(standings: DriverStandings) {
-    let mut table = Table::new();
-
-    table
-        .load_preset(comfy_table::presets::UTF8_FULL_CONDENSED)
-        .apply_modifier(UTF8_ROUND_CORNERS)
-        .apply_modifier(UTF8_SOLID_INNER_BORDERS)
-        .remove_style(TableComponent::HorizontalLines)
-        .set_content_arrangement(ContentArrangement::Dynamic)
-        .set_header(vec!["Pos", "Driver", "Team", "Points"]);
+    let mut table = build_table(vec!["Pos", "Driver", "Constructor", "Points"]);
 
     for e in standings.entries {
         let name = name_with_country_flag(
@@ -39,12 +27,13 @@ pub fn print_driver_standings_table(standings: DriverStandings) {
             &e.driver.nationality,
         );
 
-        let constructor_name: &str = match e.constructors.as_slice() {
-            [] => "Unknown",
-            [only] => &only.name,
-            [.., last] => &last.name,
+        // For cases were a driver raced for multiple constructors in a season,
+        // only display the last one.
+        let constructor_name = match e.constructors.as_slice() {
+            [] => "Unknown".to_string(),
+            [only] => clean_constructor_name(&only.name),
+            [.., last] => clean_constructor_name(&last.name),
         };
-        let constructor_name = clean_constructor_name(&constructor_name);
 
         table.add_row(vec![
             &e.position,
@@ -59,20 +48,8 @@ pub fn print_driver_standings_table(standings: DriverStandings) {
 }
 
 /// Print a pretty formatted table of F1 constructor standings to stdout.
-///
-/// # Arguments
-///
-/// * `standings` - slice of ConstructorStanding struct (position, constructor, points)
 pub fn print_constructor_standings_table(standings: ConstructorStandings) {
-    let mut table = Table::new();
-
-    table
-        .load_preset(comfy_table::presets::UTF8_FULL_CONDENSED)
-        .apply_modifier(UTF8_ROUND_CORNERS)
-        .apply_modifier(UTF8_SOLID_INNER_BORDERS)
-        .remove_style(TableComponent::HorizontalLines)
-        .set_content_arrangement(ContentArrangement::Dynamic)
-        .set_header(vec!["Pos", "Constructor", "Points"]);
+    let mut table = build_table(vec!["Pos", "Constructor", "Points"]);
 
     for e in standings.entries {
         let constructor_name = clean_constructor_name(&e.constructor.name);
@@ -83,23 +60,16 @@ pub fn print_constructor_standings_table(standings: ConstructorStandings) {
     println!("{table}\n");
 }
 
+/// Print a pretty formatted table of F1 race results to stdout.
 pub fn print_race_results_table(race_table: RaceTable) {
-    let mut table = Table::new();
-
-    table
-        .load_preset(comfy_table::presets::UTF8_FULL_CONDENSED)
-        .apply_modifier(UTF8_ROUND_CORNERS)
-        .apply_modifier(UTF8_SOLID_INNER_BORDERS)
-        .remove_style(TableComponent::HorizontalLines)
-        .set_content_arrangement(ContentArrangement::Dynamic)
-        .set_header(vec![
-            "Pos",
-            "Driver",
-            "Constructor",
-            "Time",
-            "Grid",
-            "Points",
-        ]);
+    let mut table = build_table(vec![
+        "Pos",
+        "Driver",
+        "Constructor",
+        "Time",
+        "Grid",
+        "Points",
+    ]);
 
     let leader_laps = race_table.races[0].results[0].laps.unwrap_or_default();
 
@@ -152,4 +122,19 @@ fn name_with_country_flag(first_name: &str, last_name: &str, nationality: &str) 
         "{first_name} {last_name} {}",
         FLAGS.get(nationality).map(String::as_str).unwrap_or("")
     )
+}
+
+/// Build a comfy_table for output with the provided headers.
+fn build_table(headers: Vec<&str>) -> Table {
+    let mut table = Table::new();
+
+    table
+        .load_preset(comfy_table::presets::UTF8_FULL_CONDENSED)
+        .apply_modifier(UTF8_ROUND_CORNERS)
+        .apply_modifier(UTF8_SOLID_INNER_BORDERS)
+        .remove_style(TableComponent::HorizontalLines)
+        .set_content_arrangement(ContentArrangement::Dynamic)
+        .set_header(headers);
+
+    table
 }
